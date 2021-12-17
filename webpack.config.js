633ae624a1328df92fa690b0b4e1dc9fa2b9c6bb
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2021-12-17 17:00:59 -0500

    added service-worker.js, rearranged dist folder, updated index.html to use the app.bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,9 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
 const config = {
-  entry: {
-    app: "./assets/js/index.js",
-    favorites: "./assets/js/favorites.js",
-    topic: "./assets/js/topic.js",
-  },
+  entry: "./public/index.js",
   output: {
-    path: __dirname + "/dist",
+    path: __dirname + "./public/dist",
     filename: "[name].bundle.js",
   },
   mode: "development",
@@ -29,19 +25,19 @@ const config = {
   plugins: [
     new WebpackPwaManifest({
       fingerprints: false,
-      name: "Newsy app",
-      short_name: "Newsy",
+      name: "workout tracker",
+      short_name: "workout",
       description:
-        "An application that allows you to view different news articles and save your favorites.",
+        "An application that allows you to view different transactions both online and offline.",
       background_color: "#01579b",
       theme_color: "#ffffff",
       "theme-color": "#ffffff",
       start_url: "/",
       icons: [
         {
-          src: path.resolve("assets/images/icons/android-chrome-192x192.png"),
+          src: path.resolve("public/icons/icon-192x192.png"),
           sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join("assets", "icons"),
+          destination: path.join("public", "icons"),
         },
       ],
     }),
